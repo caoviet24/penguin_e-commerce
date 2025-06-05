2a7528134a8b081e18b0276ca2513b81b6b50d84
caoviet24 <vcb.031024@gmail.com>
2025-06-06 01:54:38 +0700

    update dockerfile

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,12 +2,13 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     reactStrictMode: true,
+    output: 'standalone',
     images: {
         remotePatterns: [
             {
                 protocol: "https",
                 hostname: "**"
-            }, 
+            },
             {
                 protocol: "http",
                 hostname: "**"
